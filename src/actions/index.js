7eabe5149c29e7a3b62de1cceb54ebdce21a77a5
yurika sakae <yurika.sun@gmail.com>
2019-08-14 14:13:27 -0700

    initial commit WIP: display header using REDUX stored data

---
+++ src/actions/index.js
@@ -0,0 +1,4 @@
+export const addFeature = () => {
+  console.log("action creator was invoked");
+  return { type: "ADD_FEATURE" };
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { featureReducer } from "./reducers";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+export const store = createStore(featureReducer);
+console.log("reducer index store", store);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
